aadcefd0607d31d462fc9ac6fc29357d422d05a6
zcmiracle <489368600@qq.com>
2022-03-05 10:30:18 +0800

    依赖倒置原则

--- 05-依赖倒置原则/main.swift
+++ 05-依赖倒置原则/main.swift
@@ -5,6 +5,17 @@
 //  Created by XFB on 2022/3/4.
 //
 
+/**
+ 依赖倒置原则：高层不应该依赖于底层，两者都依赖于抽象
+ 
+ 抽象 不应该 依赖于 细节，细节依赖于抽象
+ 
+ 依赖倒置原则 实现的核心是 面向接口编程
+ 
+ 
+ */
+
+
 import Foundation
 
 //class FoodStore {
@@ -24,23 +35,34 @@ import Foundation
 //customer.shopping(store: FoodStore(), count: 3)
 
 
+
 // 优化1
-protocol StoreProtocol {
+protocol Store {
     func sell(count: Int)
 }
 
-class FoodStore1: StoreProtocol {
+// 食品商店
+class FoodStore: Store {
     func sell(count: Int) {
         print("食品商店售卖了\(count)份食品")
     }
 }
 
-class Customer1 {
-    func shopping(store: StoreProtocol, count: Int) {
+// 百货商店
+class DepartmentStore: Store {
+    func sell(count: Int) {
+        print("百货商店售卖了\(count)份百货")
+    }
+}
+
+// 顾客
+class Customer {
+    func shopping(store: Store, count: Int) {
         print("用户购物")
         store.sell(count: count)
     }
 }
 
-let customer = Customer1()
-customer.shopping(store: FoodStore1(), count: 3)
+let customer = Customer()
+customer.shopping(store: FoodStore(), count: 3)
+customer.shopping(store: DepartmentStore(), count: 5)
